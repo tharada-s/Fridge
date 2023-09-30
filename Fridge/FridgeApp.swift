33c8c43d364bb994f93248bfa78fb7da63ec3e97
tomobou5103 <local3939.info@gmail.com>
2023-09-30 15:29:42 +0900

    Initial Commit

---
+++ Fridge/FridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FridgeApp.swift
+//  Fridge
+//
+//  Created by TomoHarada on 2023/09/30.
+//
+
+import SwiftUI
+
+@main
+struct FridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
